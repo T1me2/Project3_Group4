1bac4c655950f39db9b4e62324ffa3f828e551d9
Glen Dagger <glendagger@gmail.com>
2023-01-20 10:59:12 -0600

    Add popup and zoom for county name when clicked

--- GD_work/static/js/logic.js
+++ GD_work/static/js/logic.js
@@ -145,7 +145,7 @@ function createMap(locations, newLayer) {
       fillColor: 'rgb(153,216,201)',
       weight: 2,
       opacity: 1,
-      color: 'white',
+      color: 'gray',
       dashArray: '3',
       fillOpacity: 0.7
     },
@@ -166,7 +166,7 @@ function createMap(locations, newLayer) {
             fillColor: 'rgb(153,216,201)',
             weight: 2,
             opacity: 1,
-            color: 'white',
+            color: 'gray',
             dashArray: '3',
             fillOpacity: 0.7
           })
@@ -176,12 +176,49 @@ function createMap(locations, newLayer) {
         }
       })
     }
-
   })
-  ;
+ ;
 
   // Counties Layer
-  let countyLayer = L.geoJson(countiesData);
+  let countyLayer = L.geoJson(countiesData, {
+    style: {
+      fillColor: 'rgb(229,245,249)',
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7
+    },
+    onEachFeature: (feature,layer) => {
+      console.log(feature);
+      layer.on({
+        mouseover: e => {
+          let layer = e.target
+          layer.setStyle({
+            weight: 5,
+            color: "white",
+            dashArray: '',
+            fillOpacity: 0.7
+          })
+          layer.bringToFront()
+        },
+        mouseout: e => {
+          e.target.setStyle({
+            fillColor: 'rgb(229,245,249)',
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.7
+          })
+        },
+        click: e => {
+          myMap.fitBounds(e.target.getBounds());
+        }
+      }).bindPopup(`${feature.properties.NAME} County}`);
+    }
+  })
+  ;
 
   // Create base and overlay maps
   let baseMaps = {
