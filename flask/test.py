7f72ada0296f15786bd26f59481a361bb3ed0ff9
T1me2 <jmiller10@css.edu>
2023-01-23 16:48:58 -0600

    updates

--- flask/test.py
+++ flask/test.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 
 #set up mongo atlas database connection
 conn = "mongodb+srv://project3_group4:project3_group4@cluster0.a6d7ysg.mongodb.net/?retryWrites=true&w=majority"
+# conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn)
 
 #select db and collection to use
@@ -21,12 +22,15 @@ collection = db.data
 @app.route("/api/v1.0/project3/group4/data")
 def group_data():
     """Return what we need to be json"""
-    result_list = []
-    results = list(collection.find())
-    for result in results:
-        result_list.append(result)
+    output = {}
+    i = 0
+    results = collection.find()
+    for x in results:
+        output[i] = x
+        output[i].pop('_id')
+        i += 1
     
-    return jsonify(result_list)
+    return jsonify(output)
     
     
 
