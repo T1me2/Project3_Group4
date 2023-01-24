d27518a4bb1163389bdfddfc22d58e8c5380dfd1
Glen Dagger <glendagger@gmail.com>
2023-01-23 22:52:57 -0600

    Format panel info

--- GD_work/static/js/logic.js
+++ GD_work/static/js/logic.js
@@ -7,11 +7,66 @@ let stateDict = {'01':'AL', '02':'AK', '04':'AZ', '05':'AR', '06':'CA', '08':'CO
                 '53':'WA', '54':'WV', '55':'WI', '56':'WY'
 };
 
-// Create array of state abbreviations 
-let stateNames = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 
-                  'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 
-                  'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA',
-                  'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
+// Create dictionary of state abbreviations 
+let stateNames = {
+    'AK': 'Alaska',
+    'AL': 'Alabama',
+    'AR': 'Arkansas',
+    'AS': 'American Samoa',
+    'AZ': 'Arizona',
+    'CA': 'California',
+    'CO': 'Colorado',
+    'CT': 'Connecticut',
+    'DC': 'District of Columbia',
+    'DE': 'Delaware',
+    'FL': 'Florida',
+    'GA': 'Georgia',
+    'GU': 'Guam',
+    'HI': 'Hawaii',
+    'IA': 'Iowa',
+    'ID': 'Idaho',
+    'IL': 'Illinois',
+    'IN': 'Indiana',
+    'KS': 'Kansas',
+    'KY': 'Kentucky',
+    'LA': 'Louisiana',
+    'MA': 'Massachusetts',
+    'MD': 'Maryland',
+    'ME': 'Maine',
+    'MI': 'Michigan',
+    'MN': 'Minnesota',
+    'MO': 'Missouri',
+    'MP': 'Northern Mariana Islands',
+    'MS': 'Mississippi',
+    'MT': 'Montana',
+    'NA': 'National',
+    'NC': 'North Carolina',
+    'ND': 'North Dakota',
+    'NE': 'Nebraska',
+    'NH': 'New Hampshire',
+    'NJ': 'New Jersey',
+    'NM': 'New Mexico',
+    'NV': 'Nevada',
+    'NY': 'New York',
+    'OH': 'Ohio',
+    'OK': 'Oklahoma',
+    'OR': 'Oregon',
+    'PA': 'Pennsylvania',
+    'PR': 'Puerto Rico',
+    'RI': 'Rhode Island',
+    'SC': 'South Carolina',
+    'SD': 'South Dakota',
+    'TN': 'Tennessee',
+    'TX': 'Texas',
+    'UT': 'Utah',
+    'VA': 'Virginia',
+    'VI': 'Virgin Islands',
+    'VT': 'Vermont',
+    'WA': 'Washington',
+    'WI': 'Wisconsin',
+    'WV': 'West Virginia',
+    'WY': 'Wyoming'
+}
 
 // Define toner tile baselayer
 let toner = new L.StamenTileLayer("toner-lite");
@@ -127,7 +182,7 @@ function onEachState(feature,layer) {
           layer.setStyle(stateSelectedStyle);
 
           // Update panel title to state abbr.
-          d3.select('.panel-title').text(selectedStateAbb);
+          d3.select('.panel-title').text(stateNames[selectedStateAbb]);
           
           // Check for previously selected states and reset them
           if (prevLayerClicked !== null) {
@@ -367,7 +422,7 @@ function showCounties(stateJson, map) {
                 let selectedCounty = `${feature.properties.NAME}`;
 
                 // Update panel header with county/state name
-                d3.select('.panel-title').text(`${feature.properties.NAME} County, ${selectedState}`);
+                d3.select(".panel-title").text(`${feature.properties.NAME} County, ${selectedState}`);
 
                 // Create and display marker clusters for schools within selected county
                 // showSchoolMarkers(selectedState, selectedCounty);
