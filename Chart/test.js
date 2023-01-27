08e186f576222739090066a535b62ccd341eba05
Nicholas Erdos-Thayer <76061893+Nerdosth@users.noreply.github.com>
2023-01-27 14:26:35 -0600

    check in

--- Chart/test.js
+++ Chart/test.js
@@ -1,9 +1,26 @@
-// Define URL for our rendered walkability/school GeoJSON API
-let walkabilityUrl = "countygeo_jack.json";
 
-d3.json(walkabilityUrl).then(results => {
- 
-    console.log('raw', results);
-    console.log('state', results.features[0].properties.walkability_score);
+// import Chart from '/chart.js/auto'
 
-});
+import countydata from '../countydata.json' assert { type: "json" };
+// console.log(countydata)
+
+let data = countydata.features
+console.log(data)
+
+// const stateid = "03"
+let counties_list;
+function updateChartjs (stateid) {
+    //initialize list to fill with data points
+    counties_list = []
+    
+    //loop through selected state to get data points for each county based on walkability score and total population
+    let statedata = data.filter(obj => obj.properties.STATE == stateid); 
+    for (var i = 0; i < statedata.length; i++) {
+        counties_list.push({
+            x: statedata[i].properties.population,
+            y: statedata[i].properties.walkability_score
+            });
+    }
+    console.log(counties_list)
+
+}
